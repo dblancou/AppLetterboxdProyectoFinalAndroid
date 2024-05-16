ee7d5c00b14a5a9cbb21f27a8c4e5d24173debb8
Daniel Blanco Guerrero <danielblancow@gmail.com>
2024-05-16 18:54:23 +0200

    Primer commit

---
+++ app/src/main/java/com/vedruna/appletterboxdproyectofinal/MainActivity.java
@@ -0,0 +1,42 @@
+package com.vedruna.appletterboxdproyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtener referencia a la BottomNavigationView y establecer la pestaña seleccionada por defecto
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        // Obtener el NavHostFragment y el NavController para la navegación entre fragmentos
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        // Configurar el listener de selección de elementos de la BottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            // Navegar a los fragmentos correspondientes según el elemento seleccionado
+            if (item.getItemId() == R.id.navigation_home) {
+                navController.navigate(R.id.homeFragment);
+            } else if (item.getItemId() == R.id.navigation_lists){
+                navController.navigate(R.id.listsFragment);
+            } else if (item.getItemId() == R.id.navigation_reviews){
+                navController.navigate(R.id.reviewFragment);
+            } else if (item.getItemId() == R.id.navigation_salir){
+                navController.navigate(R.id.salirFragment);
+            }
+            return true;
+        });
+    }
+}
